ff6017e03a7f9e24981bf3fb7c58a2dae921937f
seongyonglim <seongyonglim@naver.com>
2023-01-25 19:26:31 +0900

    combined edited

--- ML/Combined/run.py
+++ ML/Combined/run.py
@@ -1,8 +1,6 @@
 import imagecompare
 import detect_faces_masks
 
-# 대표이미지를 a (2).jpg로 설정하고 돌림
-
-img_name = '01.jpg'
+img_name = '01.jpg'  # 대표이미지 이름 변수
 imagecompare.main(img_name)
 detect_faces_masks.main()
--- ML/Combined/run_face_only.py
+++ ML/Combined/run_face_only.py
@@ -1,7 +1,6 @@
 import imagecompare
 import detect_faces
 
-# 대표이미지를 a (2).jpg로 설정하고 돌림
 img_name = 'c (1).jpg'
 imagecompare.main(img_name)
 detect_faces.main()
