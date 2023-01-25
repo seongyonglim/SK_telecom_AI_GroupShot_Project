44c946bcec1ef54226c9323039cb26258560608c
carrier1269 <carrier1269@naver.com>
2023-01-25 11:07:40 +0900

    2023-01-25 11:07

--- ML/Combined/run.py
+++ ML/Combined/run.py
@@ -2,6 +2,6 @@ import imagecompare
 import detect_faces_masks
 
 # 대표이미지를 a (2).jpg로 설정하고 돌림
-img_name = 'a (2).jpg'
+img_name = 'a (2).jpg' # 유저 선택 이미지
 imagecompare.main(img_name)
 detect_faces_masks.main()
