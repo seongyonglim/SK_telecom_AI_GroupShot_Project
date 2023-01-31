89f1deeb40b4d24e7f7317c111739cdca51a9e40
carrier1269 <carrier1269@naver.com>
2023-02-01 06:18:38 +0900

    get

--- ML/Combined/app.py
+++ ML/Combined/app.py
@@ -13,15 +13,19 @@ import base64
 
 app = Flask(__name__) # 플라스크 api로 던질거 설정(?) 포스트 맨으로 파일 확인할 예정
 # CORS(app) # 플라스크 function을 통해서 오류를 확인한다.
+# @app.route('/', methods=['GET'])
+# def index():
+#     # Main page
+#     return "Hello world"
 
-@app.route('/user_img', methods = ['POST']) # localurl + /user_img 에서 request설정을 통해 서버에서 flask로 사진전달
+@app.route('/deep', methods = ['POST']) # localurl + /user_img 에서 request설정을 통해 서버에서 flask로 사진전달
 def flask_to_react():
 
     # data = request.get_json()
     # path_postman = "images/postman_img/"
 
     # parsed_request = request.files.get('1')
-    f = request.files['1'] # key 1값의 value
+    f = request.files['select'] # key 1값의 value
     f.save('images/postman_img/' + secure_filename(f.filename)) # postman에서 받은 파일 폴더에 저장
     
     # parsed_request.save(path_postman)
@@ -46,24 +50,24 @@ def flask_to_react():
             #     encoded_string = base64.b64encode(img.read()).decode("utf-8")
             #     return encoded_string
             # final_bestshot_img = cv2.imread(img_path)
-            with open(img, 'rb') as img:
-                img_encoded = base64.b64encode(img.read())
+            with open(img, 'rb') as img_en:
+                img_encoded = base64.b64encode(img_en.read())
 
-            print(">"*100)
-            print(img_encoded)
+            # print(">"*100)
+            # print(img_encoded)
 
-            fp = open('tmp.txt','w')
-            fp.write(str(img_encoded))
-            fp.close()
+            # fp = open('tmp.txt','w')
+            # fp.write(str(img_encoded))
+            # fp.close()
 
             # return jsonify({"final_img" : str(img_encoded)})
-            return img_encoded
+            # return img_encoded
 
-            # return send_file(img, mimetype='image/jpg')
+            return send_file(img, mimetype='image/jpg')
     # return {"similar_img" : similar_img, "final_bestshot_img" : final_bestshot_img}
     # return jsonify({"similar" : smi_gallery , "bestshot" : final_bestshot_img})
     
     
 
-# if __name__ == "__main__":
-#     app.run(host='SKTCamServer', port='5000', debug=True)
+if __name__ == "__main__":
+    app.run(host='172.30.1.24', port='19000', debug=True)
