5b1d385459905096a9a27740c33189d02f3e96c9
WOULDU-pres <jhw664089@gmail.com>
2023-01-31 20:49:14 +0900

    code update

---
+++ front-backend-test/App.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react";
+import { Image, View, Text, StyleSheet } from "react-native";
+
+const App = () => {
+  const [base64Image, setBase64Image] = useState(null);
+
+  useEffect(() => {
+    fetch("http://172.30.1.16:5000/user_img")
+      .then((response) => response.json())
+      .then((data) => {
+        setBase64Image(data.base64Image);
+      })
+      .catch((error) => {
+        console.log("Error: ", error);
+      });
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      {base64Image ? (
+        <Image
+          source={{ uri: `data:image/png;base64,${base64Image}` }}
+          style={styles.image}
+        />
+      ) : (
+        <View style={styles.placeholder}>
+          <Text>No Image Preview</Text>
+        </View>
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100%",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+  placeholder: {
+    width: "100%",
+    height: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+export default App;
--- front-test/App.js
+++ front-test/App.js
@@ -1,28 +1,3 @@
-import React, { useState, useEffect } from "react";
-import { View, Image, Text } from "react-native";
-
-const App = () => {
-  const [imageData, setImageData] = useState({});
-
-  useEffect(() => {
-    fetch("./json_image/image_data.json")
-      .then((response) => response.json())
-      .then((data) => setImageData(data))
-      .catch((error) => console.error(error));
-  }, []);
-
-  return (
-    <View>
-      {imageData.image ? (
-        <Image
-          source={{ uri: `data:image/png;base64,${imageData.image}` }}
-          style={{ width: 200, height: 200 }}
-        />
-      ) : (
-        <Text>Loading...</Text>
-      )}
-    </View>
-  );
-};
+import App from './src/App';
 
 export default App;
